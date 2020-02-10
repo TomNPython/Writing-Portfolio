ce58a160bc7fd758d9a005625be089de74574941
TomNPython <tom.nicholson1000@gmail.com>
2020-02-10 14:33:10 +0700

    home layout set

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div class='container'>
+            <div className="row h-50">
+                <div className="col-6 d-inline-block top-left">
+                    <Link to='/'>
+                        Top Left
+                    </Link>
+                </div>
+                <div className="col-6 d-inline-block">Top Right</div>
+            </div>
+            <div className="row h-50">
+                <div className="col-6 d-inline-block">Bottom Left</div>
+                <div className="col-6 d-inline-block">Bottom Right</div>
+            </div>
         </div>
     )
 }
